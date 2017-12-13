db6eaebcb6d4a0b0e0ef765410c1e2493f91319a
thip <thip@xxx.com>
2017-12-13 10:43:15 +0700

    fix image name and update version

--- DefectRecording/DrawingViewController.swift
+++ DefectRecording/DrawingViewController.swift
@@ -53,8 +53,8 @@ class DrawingViewController: UIViewController {
     func initialUI(){
         mainImgView.image = screenShotImg
         tempImgView.image = screenShotImg
-        let cancelBarBtn = UIBarButtonItem(image:UIImage(named: "clear-button.png"), style: .plain, target: self, action: #selector(self.cancelDrawing))
-        let nextBarBtn = UIBarButtonItem(image: UIImage(named: "checked.png"), style: .plain, target: self, action: #selector(self.nextView))
+        let cancelBarBtn = UIBarButtonItem(image:UIImage(named: "clear-button@2x.png"), style: .plain, target: self, action: #selector(self.cancelDrawing))
+        let nextBarBtn = UIBarButtonItem(image: UIImage(named: "checked@2x.png"), style: .plain, target: self, action: #selector(self.nextView))
         self.navigationItem.leftBarButtonItem = cancelBarBtn
         self.navigationItem.leftBarButtonItem?.tintColor = UIColor.white
         self.navigationItem.rightBarButtonItem = nextBarBtn
